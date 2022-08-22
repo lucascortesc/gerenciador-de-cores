2964ae214b66931553972b6fd6762ad1d0a3adaf
lucascortesc <ccordeiro.lucas@gmail.com>
2022-08-21 23:42:41 -0300

    fix: hex validation

--- functions/src/validation/palettes/index.ts
+++ functions/src/validation/palettes/index.ts
@@ -19,7 +19,7 @@ export const createPaletteSchema = yup
               .string()
               .required("color hex is required")
               .min(1, "color hex must contain at least 1 characters")
-              .max(6, "color hex must contain less than 7 characters"),
+              .max(7, "color hex must contain less than 8 characters"),
           })
           .noUnknown(true)
       )
@@ -44,7 +44,7 @@ export const updatePaletteSchema = yup
             .string()
             .required("color hex is required")
             .min(1, "color hex must contain at least 1 characters")
-            .max(6, "color hex must contain less than 7 characters"),
+            .max(7, "color hex must contain less than 8 characters"),
         })
         .noUnknown(true)
     ),
